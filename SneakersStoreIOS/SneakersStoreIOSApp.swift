4f8021878bb43beb3ac7997ea9455dc90994b52f
turnMeUpSon <milleryan2003@gmail.com>
2024-02-19 21:06:38 +0000

    Initial Commit

---
+++ SneakersStoreIOS/SneakersStoreIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakersStoreIOSApp.swift
+//  SneakersStoreIOS
+//
+//  Created by Ian Miller on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SneakersStoreIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
